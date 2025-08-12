117c979a42f0573074177d38eff7ed0e97ad260b
MayureshGadkari <gadkarimayuresh97@gmail.com>
2025-08-12 16:02:10 +0530

    Group Anagram

--- easy/medium/abc.py
+++ easy/medium/abc.py
@@ -1,1 +1,22 @@
-print("Hello")
+'''Input: strs = ["act","pots","tops","cat","stop","hat"]
+
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+'''
+strs = ["act","pots","tops","cat","stop","hat"]
+
+def find_grp_anagram(strs):
+    hashmap ={}
+
+    for i in strs:
+        to_check = tuple(sorted(i))
+        if to_check not in hashmap:
+            hashmap[to_check]=[i]
+        else:
+            hashmap[to_check].append(i)
+    return list(hashmap.values())
+
+        #print(tuple(sorted(i)))
+print(find_grp_anagram(strs))
+
+
+
